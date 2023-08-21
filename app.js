f2d912c126ea673373b45f95c2345659398c9bcd
Prodigy <124164910+P-r-o-d-i-g-y@users.noreply.github.com>
2023-08-21 14:36:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -60,47 +60,47 @@ $(document).ready(function() {
 
 
 // // регистрация плагинов
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
 
 
 // для телефонов условие
-// if (ScrollTrigger.isTouch !==1) { //определение телефон или нет
-//     ScrollSmoother.create({
-//         wrapper : '.wrapper', //оболочка
-//         content: '.content', //контент в оболочке
-//         smooth: 1.5,
-//         effects:true
+if (ScrollTrigger.isTouch !==1) { //определение телефон или нет
+    ScrollSmoother.create({
+        wrapper : '.wrapper', //оболочка
+        content: '.content', //контент в оболочке
+        smooth: 1.5,
+        effects:true
     
-//     });
-//     var slideIndex = 1;
-//     showSlides(slideIndex);
+    });
+    var slideIndex = 1;
+    showSlides(slideIndex);
 
-//     function plusSlides(n) {
-//     showSlides(slideIndex += n);
-//     }
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
 
-//     function currentSlide(n) {
-//     showSlides(slideIndex = n);
-//     }
+    function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
 
-//     function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) {slideIndex = 1}    
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";  
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex-1].style.display = "block";  
-//     dots[slideIndex-1].className += " active";
-//     }
+    function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    }
     
-// }
+}
 
 
 
